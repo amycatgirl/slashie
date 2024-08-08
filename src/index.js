967d2c64d198d1f094ffea6d68ad9b73785a0093
Amy <amy@amogus.cloud>
2024-08-07 21:18:19 -0400

    init repo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 () => {
-  // This is your plugin's entrypoint, code you type here will be ran as soon as the client starts up
-  // regardless of whether the user's session started.
+	async function main() {
+		const client = new Promise((resolve) => {
+			setTimeout(() => {
+				// I assume you have a good, stable connection
+				// if you don't then too bad, workarounds suck
+				resolve(window.controllers.client.getAvailableClient())
+			}, 2500)
+		})
 
-  console.log("Heyo! I am a sample plugin!");
+		client.then(c => c.on("message", (m) => {
+			if (m.author.id === c.user.id && m.content.startsWith("/me")) {
+				m.edit({ content: `*${(c.user.display_name ?? c.user.username).trim()} ${m.content.substring(4).trim()}*`})
+			}
+		}))
+	}
+
+	main()
 }
